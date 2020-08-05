4faff4c2a69f03e3497e5994364f14decdcd0780
leonardo studart <lstudart@pyx4.com>
2020-08-04 22:08:36 -0400

    feat: create index  method on ClassesController

--- src/controllers/classes-controller.ts
+++ src/controllers/classes-controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express'
 import db from '../database/connection'
 import convertHoutToMinutes from '../utils/convert-hours-to-minutes'
 
+
 interface ScheduleItem {
   week_day: number,
   from: string,
@@ -11,6 +12,38 @@ interface ScheduleItem {
 
 
 export default class ClassesController {
+  async index(req: Request, res: Response) {
+    const filters = req.query
+
+    const subject = filters.subject as string
+    const time = filters.time as string
+    const week_day = filters.week_day as string
+
+    if (!filters.week_day || !filters.subject || !filters.time) {
+      return res.status(400).json({
+        error: "Missing filters to search classes"
+      })
+    }
+
+    const timeInMinutes = convertHoutToMinutes(time)
+    const classes = await db('classes')
+      .whereExists(function() {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??',[Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??',[timeInMinutes])
+          .whereRaw('`class_schedule`.`from` > ??',[timeInMinutes])
+
+      })
+      .where('classes.subject', '=', subject)
+      .join('users','classes.user_id','=','users.id')
+      .select(['classes.*','users.*'])
+
+    return res.json(classes)
+
+  }
+
   async create(req: Request, res: Response) {
     const {
       name,
@@ -18,14 +51,13 @@ export default class ClassesController {
       whatsapp,
       bio,
       subject,
-      cost, 
+      cost,
       schedule
     } = req.body
-  
-  
+
+
     const trx = await db.transaction();
-  
-  
+
     try {
       const insertedUsersIds = await trx('users').insert({
         name,
@@ -33,40 +65,40 @@ export default class ClassesController {
         whatsapp,
         bio
       })
-    
+
       const user_id = insertedUsersIds[0]
-    
+
       const insertedClassesIds = await trx('classes').insert({
         subject,
         cost,
         user_id
       })
-    
+
       const class_id = insertedClassesIds[0]
-    
-      const classSechedule = schedule.map(({week_day, from, to}: ScheduleItem) => {
-    
-        return  {
+
+      const classSechedule = schedule.map(({ week_day, from, to }: ScheduleItem) => {
+
+        return {
           class_id,
           week_day,
           from: convertHoutToMinutes(from),
           to: convertHoutToMinutes(to)
         }
       })
-    
+
       await trx('class_schedule').insert(classSechedule)
-    
+
       await trx.commit()
-    
+
       return res.status(201).send()
-    }catch (err) {
+    } catch (err) {
       trx.rollback()
       console.log(err);
-      
+
       return res.status(400).json({
         error: 'Unexpected error while creating new class'
       })
     }
-    
+
   }
 }
